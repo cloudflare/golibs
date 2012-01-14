64f4a556ced34000378b8b44266ad753abe86529
Francisco Souza <f@souza.cc>
2012-01-14 12:55:25 -0200

    Opening for write and close a database file

--- kc/kc.go
+++ kc/kc.go
@@ -1,10 +1,42 @@
 package kc
 
-type DB string
+/*
+#cgo CFLAGS: -I/usr/local/include
+#cgo LDFLAGS: -L/usr/local/lib -lkyotocabinet
+# include <kclangc.h>
+*/
+import "C"
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+type KCError string
+
+func (err KCError) Error() string {
+	return string(err)
+}
+
+type DB struct {
+	db *C.KCDB
+}
 
 func (d *DB) Close() {
+	C.kcfree(unsafe.Pointer(d.db))
 }
 
 func OpenForWriting(dbfilepath string) (*DB, error) {
-	return nil, nil
+	db := &DB{db: C.kcdbnew()}
+
+	dbname := C.CString(dbfilepath)
+	defer C.free(unsafe.Pointer(dbname))
+
+	if (C.kcdbopen(db.db, dbname, C.KCOWRITER | C.KCOCREATE) == 0) {
+		errMsg := C.GoString(C.kcecodename(C.kcdbecode(db.db)))
+		err := KCError(fmt.Sprintf("Error opening the %s file: %s", dbfilepath, errMsg))
+		return nil, err
+	}
+
+	return db, nil
 }
--- kc/kc_test.go
+++ kc/kc_test.go
@@ -7,13 +7,22 @@ import (
 
 func Exists(path string) bool {
 	_, err := os.Stat(path)
-	return err.(*os.PathError).Err != os.ENOENT
+	return err == nil || err.(*os.PathError).Err != os.ENOENT
+}
+
+func Remove(path string) {
+	if Exists(path) {
+		os.Remove(path)
+	}
 }
 
 func TestShouldCreateTheFileOnDiscWhenOpenForWriting(t* testing.T) {
 	filepath := "/tmp/names.kch"
+	defer Remove(filepath)
+
 	db, _ := OpenForWriting(filepath)
 	defer db.Close()
+
 	if !Exists(filepath) {
 		t.Errorf("%s should exists, but it doesn't", filepath)
 	}
