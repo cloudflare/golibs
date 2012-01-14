01473824b7680f3077206618e5d4c7fbfd24b4d7
Francisco Souza <f@souza.cc>
2012-01-14 01:58:59 -0200

    First failing test
    The function OpenForWriting and the Close method exist only to get a
    code that compiles, but nothing is working yet.

--- kc/kc.go
+++ kc/kc.go
@@ -1,1 +1,11 @@
 package kc
+
+type DB string
+
+func (d *DB) Close() error {
+	return nil
+}
+
+func OpenForWriting(dbfilepath string) (*DB, error) {
+	return nil, nil
+}
--- kc/kc_test.go
+++ kc/kc_test.go
@@ -1,1 +1,20 @@
 package kc
+
+import (
+	"os"
+	"testing"
+)
+
+func Exists(path string) bool {
+	_, err := os.Stat(path)
+	return err.(*os.PathError).Err != os.ENOENT
+}
+
+func TestShouldCreateTheFileOnDiscWhenOpenForWriting(t* testing.T) {
+	filepath := "/tmp/names.kch"
+	db, _ := OpenForWriting(filepath)
+	defer db.Close()
+	if !Exists(filepath) {
+		t.Errorf("%s should exists, but it doesn't", filepath)
+	}
+}
