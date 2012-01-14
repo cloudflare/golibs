1accad358e71e895fd30292a26a2a7ba55009f88
Francisco Souza <f@souza.cc>
2012-01-14 13:46:49 -0200

    Getting and setting values in the database

--- kc/kc.go
+++ kc/kc.go
@@ -22,21 +22,65 @@ type DB struct {
 	db *C.KCDB
 }
 
+func (d *DB) LastError() string {
+	errMsg := C.GoString(C.kcecodename(C.kcdbecode(d.db)))
+	return errMsg
+}
+
+func (d *DB) Set(key, value string) error {
+	cKey := C.CString(key)
+	defer C.free(unsafe.Pointer(cKey))
+
+	cValue := C.CString(value)
+	defer C.free(unsafe.Pointer(cValue))
+
+	lKey := C.size_t(len(key))
+	lValue := C.size_t(len(value))
+
+	if (C.kcdbset(d.db, cKey, lKey, cValue, lValue) == 0) {
+		errMsg := d.LastError()
+		err := KCError(fmt.Sprintf("Failed to set the value %s to the key %s: %s", value, key, errMsg))
+		return err
+	}
+
+	return nil
+}
+
+func (d *DB) Get(key string) (string, error) {
+	var resultLen C.size_t
+
+	cKey := C.CString(key)
+	defer C.free(unsafe.Pointer(cKey))
+
+	lKey := C.size_t(len(key))
+
+	cValue := C.kcdbget(d.db, cKey, lKey, &resultLen)
+	defer C.kcfree(unsafe.Pointer(cValue))
+
+	if (cValue == nil) {
+		errMsg := d.LastError()
+		err := KCError(fmt.Sprintf("Failed to get a value for the key %s: %s", key, errMsg))
+		return "", err
+	}
+
+	return C.GoString(cValue), nil
+}
+
 func (d *DB) Close() {
 	C.kcfree(unsafe.Pointer(d.db))
 }
 
 func OpenForWrite(dbfilepath string) (*DB, error) {
-	db := &DB{db: C.kcdbnew()}
+	d := &DB{db: C.kcdbnew()}
 
 	dbname := C.CString(dbfilepath)
 	defer C.free(unsafe.Pointer(dbname))
 
-	if (C.kcdbopen(db.db, dbname, C.KCOWRITER | C.KCOCREATE) == 0) {
-		errMsg := C.GoString(C.kcecodename(C.kcdbecode(db.db)))
-		err := KCError(fmt.Sprintf("Error opening the %s file: %s", dbfilepath, errMsg))
+	if (C.kcdbopen(d.db, dbname, C.KCOWRITER | C.KCOCREATE) == 0) {
+		errMsg := d.LastError()
+		err := KCError(fmt.Sprintf("Error opening %s: %s", dbfilepath, errMsg))
 		return nil, err
 	}
 
-	return db, nil
+	return d, nil
 }
--- kc/kc_test.go
+++ kc/kc_test.go
@@ -1,7 +1,9 @@
 package kc
 
 import (
+	"fmt"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -16,8 +18,8 @@ func Remove(path string) {
 	}
 }
 
-func TestShouldCreateTheFileOnDiscWhenOpenForWrite(t* testing.T) {
-	filepath := "/tmp/names.kch"
+func TestShouldCreateTheFileInTheDiscWhenOpenForWrite(t* testing.T) {
+	filepath := "/tmp/musicians.kch"
 	defer Remove(filepath)
 
 	db, _ := OpenForWrite(filepath)
@@ -25,5 +27,48 @@ func TestShouldCreateTheFileOnDiscWhenOpenForWrite(t* testing.T) {
 
 	if !Exists(filepath) {
 		t.Errorf("%s should exists, but it doesn't", filepath)
+	}
+}
+
+func TestShouldReportADescriptiveErrorMessageWhenFailToOpenADatabaseForWrite(t *testing.T) {
+	filepath := "/root/db.kch" // i won't be able to write here :)
+	expectedMessagePart := fmt.Sprintf("Error opening %s:", filepath)
+
+	_, err := OpenForWrite(filepath)
+
+	if (err == nil || !strings.Contains(err.Error(), expectedMessagePart)) {
+		t.Errorf("Should fail with a descriptive message")
+	}
+}
+
+func TestShouldBeAbleToSetAndGetAValue(t* testing.T) {
+	filepath := "/tmp/musicians.kch"
+	defer Remove(filepath)
+
+	if db, err := OpenForWrite(filepath); err == nil {
+		defer db.Close()
+
+		db.Set("name", "Alanis Morissette")
+		if name, _ := db.Get("name"); name != "Alanis Morissette" {
+			t.Errorf("Should set the value \"Alanis Morissette\" to the key \"name\", but got \"%s\".", name)
+		}
+	} else {
+		t.Errorf("Failed to open the file: %s.", filepath)
+	}
+}
+
+func TestShouldReturnErrorExplainingWhenAKeyIsNotFound(t *testing.T) {
+	filepath := "/tmp/musicians.kch"
+	defer Remove(filepath)
+
+	if db, err := OpenForWrite(filepath); err == nil {
+		defer db.Close()
+
+		_, err := db.Get("name")
+		if err == nil || !strings.Contains(err.Error(), "no record"){
+			t.Errorf("Should return a clear error message when no record is found for a key.")
+		}
+	} else {
+		t.Errorf("Failed to open the file: %s.", filepath)
 	}
 }
