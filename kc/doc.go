ab56fecbf9d9d0a60097a0249752c1cb0bba6eff
Francisco Souza <f@souza.cc>
2012-03-25 01:57:58 -0300

    license: added license file and license header to source files

--- kc/doc.go
+++ kc/doc.go
@@ -1,3 +1,7 @@
+// Copyright 2012 Francisco Souza. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 /*
 Package kc provides types, methods and functions to interact with an
 Kyoto Cabinet database.
