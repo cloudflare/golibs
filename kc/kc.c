7537a9f5aa46aeaa08a86498483b3360cc62775d
Francisco Souza <f@souza.cc>
2013-01-12 19:24:49 -0200

    kc: implement match_prefix function using C
    Using C so the code don't look too ugly. This function wraps
    kcdbmatchprefix function.

--- kc/kc.c
+++ kc/kc.c
@@ -1,4 +1,4 @@
-// Copyright 2012 gokabinet authors. All rights reserved.
+// Copyright 2013 gokabinet authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -13,4 +13,62 @@ gokccurget(KCCUR *cur)
 	p.key.buf = kccurget(cur, &p.key.size, &argvbuf, &p.value.size, 1);
 	p.value.buf = (char *)argvbuf;
 	return p;
+}
+
+void
+_alloc(char ***v, size_t n)
+{
+	int i;
+	*v = (char **)malloc(n * sizeof(char *));
+	for(i = 0; i < n; ++i) {
+		(*v)[i] = (char *)malloc(MAX_RECORD_SIZE * sizeof(char));
+	}
+}
+
+void
+_free(char ***v, size_t n)
+{
+	int i;
+	for(i = 0; i < n; ++i) {
+		free((*v)[i]);
+	}
+	free(*v);
+}
+
+struct strary
+match_prefix(KCDB *db, char *prefix, size_t max)
+{
+	int i;
+	int64_t n;
+	struct strary s;
+	_alloc(&s.v, max);
+	n = kcdbmatchprefix(db, prefix, s.v, max);
+	if(n == -1) {
+		_free(&s.v, max);
+		s.v = nil;
+		return s;
+	}
+	s.n = n;
+	if(n < max) {
+		for(i = n; i < max; ++i) {
+			free(s.v[i]);
+		}
+		s.v = (char **)realloc(s.v, s.n * sizeof(char *));
+	}
+	return s;
+}
+
+char *
+strary_item(struct strary *s, int64_t position)
+{
+	if(position < s->n) {
+		return s->v[position];
+	}
+	return nil;
+}
+
+void
+free_strary(struct strary *s)
+{
+	_free(&s->v, s->n);
 }
--- kc/kc.h
+++ kc/kc.h
@@ -1,5 +1,18 @@
-// Copyright 2012 gokabinet authors. All rights reserved.
+// Copyright 2013 gokabinet authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+#define MAX_RECORD_SIZE 1024
+#define nil 0
+
+struct strary
+{
+	char    **v;
+	int64_t n;
+};
+
+// strary_item returns the item at the given position.
+char *strary_item(struct strary *s, int64_t position);
 KCREC gokccurget(KCCUR *cur);
+struct strary match_prefix(KCDB *db, char *prefix, size_t max);
+void free_strary(struct strary *s);
