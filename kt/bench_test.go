c5974df440995cc121f184bf3342dc131297b17b
Daniel Morsing <daniel@cloudflare.com>
2015-02-01 12:32:12 +0000

    add benchmark for gets.

--- kt/bench_test.go
+++ kt/bench_test.go
@@ -27,3 +27,37 @@ func BenchmarkSetLarge(b *testing.B) {
 		conn.Set(str, large[:])
 	}
 }
+
+func BenchmarkGet(b *testing.B) {
+	cmd := startServer(b)
+	defer haltServer(cmd, b)
+	conn := NewConn(KTHOST, KTPORT, 1, DEFAULT_TIMEOUT)
+	err := conn.Set("something", []byte("foobar"))
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := conn.Get("something")
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkGetLarge(b *testing.B) {
+	cmd := startServer(b)
+	defer haltServer(cmd, b)
+	conn := NewConn(KTHOST, KTPORT, 1, DEFAULT_TIMEOUT)
+	err := conn.Set("something", make([]byte, 4096))
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := conn.Get("something")
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
