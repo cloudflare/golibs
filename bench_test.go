6ccdccc1c1bb76a18d1d5cc5f38d60f839a9ec4d
Daniel Morsing <daniel@cloudflare.com>
2015-02-25 12:02:27 +0000

    add benchmarks for get bulk bytes

--- bench_test.go
+++ bench_test.go
@@ -1,6 +1,7 @@
 package kt
 
 import (
+	"fmt"
 	"strconv"
 	"testing"
 )
@@ -68,6 +69,31 @@ func BenchmarkGetLarge(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		_, err := conn.Get("something")
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkBulkBytes(b *testing.B) {
+	cmd := startServer(b)
+	defer haltServer(cmd, b)
+	db, err := NewConn(KTHOST, KTPORT, 1, DEFAULT_TIMEOUT)
+	if err != nil {
+		b.Fatal(err.Error())
+	}
+
+	keys := make(map[string][]byte)
+	for i := 0; i < 200; i++ {
+		keys[fmt.Sprintf("cache/news/%d", i)] = []byte{'4'}
+	}
+
+	for k := range keys {
+		db.Set(k, []byte("something"))
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		err := db.GetBulkBytes(keys)
 		if err != nil {
 			b.Fatal(err)
 		}
