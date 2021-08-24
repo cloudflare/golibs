cloudflare/golibs

commit 3c537d58cbb0d3c7df2081be376f581a00640d3d

kt: allow use of kubernetes pki file paths transparently
Certificates can be in different paths depending on what is managing them.
While it is possible to make kubernets to relocate paths to match with what
this library expects, it is much nicer if the library can search for well
known file paths.  I admit, this is purely convenience.

Signed-off-by: Sami Kerola <kerolasa@iki.fi>

kt/kt.go
@@ -107,10 +107,33 @@ func expiryCertMetric(certFile string) error {
 	}
 }
 
+func certPaths(dir string) (cert, key, ca string, err error) {
+	certSets := [][]string{
+		[]string{"service.pem", "service-key.pem", "ca.pem"}, // certmgr
+		[]string{"tls.crt", "tls.key", "ca.crt"},             // kubernetes pki
+	}
+
+	for _, set := range certSets {
+		goodSet := true
+		for _, file := range set {
+			check := path.Join(dir, file)
+			if _, err := os.Stat(check); os.IsNotExist(err) {
+				goodSet = false
+				break
+			}
+		}
+		if goodSet {
+			return set[0], set[1], set[2], nil
+		}
+	}
+	return "", "", "", fmt.Errorf("there are no certificates in path: %s", dir)
+}
+
 func loadCerts(creds string) (*tls.Certificate, *x509.CertPool, error) {
-	cert := path.Join(creds, "service.pem")
-	key := path.Join(creds, "service-key.pem")
-	ca := path.Join(creds, "ca.pem")
+	cert, key, ca, err := certPaths(creds)
+	if err != nil {
+		return nil, nil, err
+	}
 
 	err := expiryCertMetric(cert)
 	if err != nil {

commit c4616e3a8141e6a4106e0f1b0d688a9773e5bc0d

do not create new 'err' when there is already one
This fixes following compiler error.

    kt/kt.go:138:6: no new variables on left side of :=

And add the missing import.

Fixes: 3c537d58cbb0d3c7df2081be376f581a00640d3d
Signed-off-by: Sami Kerola <kerolasa@iki.fi>

kt/kt.go
@@ -11,6 +11,7 @@ import (
 	"net"
 	"net/http"
 	"net/url"
+	"os"
 	"path"
 	"strconv"
 	"strings"
@@ -135,7 +136,7 @@ func loadCerts(creds string) (*tls.Certificate, *x509.CertPool, error) {
 		return nil, nil, err
 	}
 
-	err := expiryCertMetric(cert)
+	err = expiryCertMetric(cert)
 	if err != nil {
 		return nil, nil, err
 	}